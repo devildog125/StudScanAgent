8d409ba330b4e6e773bbdb2b6b53e082dcfd92b9
devildog125 <devildog125@gmail.com>
2026-06-26 22:29:08 -0500

    feat: add LangChain dependencies and initialize environment variables for agent setup

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.chat_models import init_chat_model
+from langchain_ollama import ChatOllama
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage, SystemMessage, ToolMessage
+from langsmith import traceable
+
+
+
 def main():
     print("Hello from studscanagent!")
 
